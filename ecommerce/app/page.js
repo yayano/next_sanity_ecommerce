efd1fe8207095967c3069422cb50d9463107cc73
yayano <boussetta13yahia@gmail.com>
2024-03-02 22:41:54 +0000

    from scratch to the hero banner

---
+++ ecommerce/app/page.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Product, HeroBanner, FooterBanner } from "../components";
+import { client } from "@/sanity/lib/client";
+
+export default async function Page() {
+  const { product, bannerData } = await getServerSideData();
+
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling products</h2>
+        <p>Speakers of many variations </p>
+      </div>
+      <div className="products-container">
+        {product?.map((product) => product.name)}
+      </div>
+      <FooterBanner />
+    </>
+  );
+}
+export async function getServerSideData() {
+  const query = `*[_type== 'product']`;
+  const product = await client.fetch(query);
+  const bannerQuery = `*[_type== 'banner']`;
+  const bannerData = await client.fetch(bannerQuery);
+  return { product, bannerData };
+}
---
+++ ecommerce/sanity/env.js
@@ -0,0 +1,6 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-01";
+
+export const dataset = "production";
+export const projectId = "u8o8ca0b";
+export const useCdn = true;
